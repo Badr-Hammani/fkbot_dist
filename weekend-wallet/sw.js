6b75d2619b56a179279ffbc1f14f83fc6d5a30fa
Badr-Hammani <hammaniest@gmail.com>
2026-08-30 17:23:02 +0100

    Publish borrowing log date editing v7.2

--- weekend-wallet/sw.js
+++ weekend-wallet/sw.js
@@ -1,5 +1,5 @@
 ﻿// Weekend Wallet Service Worker — Offline Caching
-const CACHE_NAME = 'weekend-wallet-v7.1';
+const CACHE_NAME = 'weekend-wallet-v7.2';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
