a91904d8ac998b9e4d4620bbdaf7abf33ea97f0d
Badr-Hammani <hammaniest@gmail.com>
2026-08-30 18:06:46 +0100

    Publish Weekend Wallet v7.3 UI improvements

--- weekend-wallet/sw.js
+++ weekend-wallet/sw.js
@@ -1,5 +1,5 @@
 ﻿// Weekend Wallet Service Worker — Offline Caching
-const CACHE_NAME = 'weekend-wallet-v7.2';
+const CACHE_NAME = 'weekend-wallet-v7.3';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
